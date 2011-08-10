bd212e2a2cec08f66640ffa96f7686e13739cffe
Delon Newman <delon.newman@gmail.com>
2011-08-09 15:52:20 -1000

    add test suite

---
+++ test/suite.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+
+$:.unshift('.') unless $:.include?('.')
+
+MiniTest::Unit.autorun
+
+require 'test/protocol/basic'
+require 'test/protocol/dsl'
+require 'test/protocol/request'
+require 'test/protocol/response'
